9903efa5f28aa81012a84ed22b15ea435c310410
zhaiyongtao <501671900@qq.com>
2021-06-09 14:14:30 +0800

    【FIX】Bind的实现

--- Pages/this指向/Bind的实现.js
+++ Pages/this指向/Bind的实现.js
@@ -6,52 +6,52 @@
  * @LastEditTime: 2021/6/8 4:31 下午
  */
 
-Function.prototype.mybind = function (oThis) {
+            Function.prototype.mybind = function (oThis) {
 
-    if (typeof this != 'function') {
+                if (typeof this != 'function') {
 
-        throw 'caller must be a function'
+                    throw 'caller must be a function'
 
-    }
+                }
 
-    let fThis = this
-    console.log('fThis', fThis)
+                let fThis = this
+                console.log('fThis', fThis)
 
-    //Array.prototype.slice.call 将类数组转为数组
+                //Array.prototype.slice.call 将类数组转为数组
 
-    let arg = Array.prototype.slice.call(arguments, 1)
+                let arg = Array.prototype.slice.call(arguments, 1)
 
-    let NOP = function () {
-    }
+                let NOP = function () {
+                }
 
-    let fBound = function () {
+                let fBound = function () {
 
-        let arg_ = Array.prototype.slice.call(arguments)
+                    let arg_ = Array.prototype.slice.call(arguments)
 
-        // new 绑定等级高于显式绑定
+                    // new 绑定等级高于显式绑定
 
-        // 作为构造函数调用时，保留指向不做修改
+                    // 作为构造函数调用时，保留指向不做修改
 
-        // 使用 instanceof 判断是否为构造函数调用
-        console.log('this', this)
+                    // 使用 instanceof 判断是否为构造函数调用
+                    console.log('this', this)
 
-        return fThis.apply(this instanceof fBound ? this : oThis, arg.concat(arg_))
+                    return fThis.apply(this instanceof fBound ? this : oThis, arg.concat(arg_))
 
-    }
+                }
 
-    // 维护原型
+                // 维护原型
 
-    if (this.prototype) {
+                if (this.prototype) {
 
-        NOP.prototype = this.prototype
+                    NOP.prototype = this.prototype
 
-    }
+                }
 
-    fBound.prototype = new NOP()
+                fBound.prototype = new NOP()
 
-    return fBound
+                return fBound
 
-}
+            }
 
 Function.prototype.myBind = function() {
     var _this = this;
@@ -82,23 +82,24 @@ function addWithThis() {
 }
 
 Function.prototype.myNewBind = function (context,...args) {
-    console.log('context',context)
-    console.log('args',args)
+    // console.log('context',context)
+    // console.log('args',args)
     let _this = this // 这里是为了保存需要绑定的this上下文，为了防止返回的函数中的this指向出现问题。如果使用箭头函数就不需要再进行一次保存了
     console.log(this) // 这个this指向调用myNewBind的方法，遵循的是谁调用我，我内部的this的指向就指向谁
     return function () {
-        return _this.apply(context,args)
+         return _this.apply(context,args)
     }
 }
 
 // const result2 = addWithThis.myNewBind(obj, 1,2,3)()
 
-// const resultFn2 = addWithThis.myNewBind(obj, 1,2,3)
+const resultFn2 = addWithThis.myNewBind(obj, 1,2,3)
+let fn2 = new resultFn2()
 // resultFn2()
-const resultFn3 = addWithThis.mybind(obj)
-resultFn3()
+// const resultFn3 = addWithThis.mybind(obj)
+// resultFn3()
 // const rf = new resultFn2()
-const rf1 = new resultFn3()
+// const rf1 = new resultFn3()
 
 // console.log('result2', result2)
 // const result1 = addWithThis.myBind(obj,1,3)()
