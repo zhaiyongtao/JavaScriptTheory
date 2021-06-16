84ac391344b2c6185bb4b28f4c126e43e3327214
zhaiyongtao <501671900@qq.com>
2021-06-17 00:41:20 +0800

    【FIX】原型继承

--- Pages/this指向/call的用法.js
+++ Pages/this指向/call的用法.js
@@ -6,21 +6,24 @@
  * @LastEditTime: 2021/6/2 6:33 下午
  */
 
-function parentFn () {
+function parentFn() {
     this.a = 1;
-    this.print= function (){
+    this.printFn = function () {
         console.log('parentFn', this.a)
     }
     // console.log(this)
 }
-parentFn()
- function childFn() {
-    this.a = 2
-     parentFn.call(this)
-     // console.log(this) // 指向全局作用域
-     this.print() // 输出parentFn
- }
 
+parentFn()
+
+function childFn() {
+    this.a = 2
+    parentFn.call(this) // 这个相当于在全局作用域下执行 parentFn()
+    console.log(this) // 指向全局作用域,全局作用下的a变量值为1，因为parentFn.call(this)这行代码对a重新赋值了
+    this.printFn() // 输出parentFn 1
+    console.log(this.a) // 1
+}
+// console.log(global.a)
 childFn()
 
 
---
+++ Pages/原型和原型对象和原型链/原型继承.js
@@ -0,0 +1,53 @@
+function Student (props) {
+    this.name = props.name || 'UnNamed'
+}
+
+Student.prototype.sayHello = function () {
+    console.log(`hello ${this.name}`) // hello Student
+}
+
+const stu = new Student({name: 'Student'})
+stu.sayHello()
+
+function StudentA (props) {
+    this.sex = props.sex || 'StudentA sex is unknown';
+}
+
+
+StudentA.prototype.getSex = function () {
+    console.log(`StudentA sex is ${this.sex}`) // StudentA sex is male
+}
+
+const stuA = new StudentA({name: 'stuA', sex: 'male'})
+
+stuA.getSex()
+
+
+function StudentAcallStudent (props) {
+    // 通过call的特性使Student内部的this指向当前函数的this
+    Student.call(this,props)// 因为new的操作导致StudentAcallStudent构造函数的内部this指向的是new操作之后返回的实例对象
+    this.sex = props.sex || 'StudentAcallStudent sex is unknown';
+}
+
+StudentAcallStudent.prototype.getSex = function () {
+    console.log(`StudentAcallStudent sex is ${this.sex}`) // StudentA sex is male
+}
+const newStuA = new StudentAcallStudent({name: 'newStuA', sex: 'female'})
+
+console.log(newStuA.name) // newStuA
+console.log(newStuA.getSex()) // StudentAcallStudent sex is female
+console.log(newStuA.sayHello()) // 报错，因为sayHello是Student原型对象上的方法，无法通过call让newStuA也具有这个方法，所以这不是继承
+
+// newStuA具有name属性的原理
+// function test (a) {
+//     this.a = a
+// }
+//
+// let obj = {
+//     b:2,
+//     c:3
+// }
+//
+// test.call(obj,1)
+//
+// console.log(obj.a) // 1
