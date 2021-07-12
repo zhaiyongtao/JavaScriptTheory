dbff9c57356df7ec2f31feec909035548205791c
zhaiyongtao <501671900@qq.com>
2021-07-13 00:01:28 +0800

    Fix: 设计模式代码

--- Pages/设计模式/发布-订阅模式.js
+++ Pages/设计模式/发布-订阅模式.js
@@ -2,6 +2,11 @@
 class  EventEmitter {
 
     // 创建单例
+    // 1) 创建一个静态变量 instance （承载实例对象）
+    // 2) 创建一个静态方法
+    // 3) 在静态方法中判断instance是否被赋值
+    // 4) 如果没有被赋值，那么对我们的类进行初始化（new），实例对象赋值给instance
+    // 5) 实例方法返回我们的是实例对象
     static eventBusInstance = null
     static EventBus() {
         if (!this.eventBusInstance) {
